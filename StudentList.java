0553baa9cb9063b48cba36ad6b9b90851b8cd477
Shahriar Hasan <shahriar19735@gmail.com>
2022-11-19 21:27:56 +0600

    second commit

--- StudentList.java
+++ StudentList.java
@@ -40,8 +40,8 @@ public class StudentList {
 			  BufferedWriter s = new BufferedWriter(
 					new FileWriter("students.txt", true));
 			  String t = args[0].substring(1);
-	          Date d = new Date();
-	          String df = "dd/mm/yyyy-hh:mm:ss a";
+			  Date d = new Date();
+			  String df = "dd/mm/yyyy-hh:mm:ss a";
 			  DateFormat dateFormat = new SimpleDateFormat(df);
 	          String fd= dateFormat.format(d);
 			  s.write(", "+t+"\nList last updated on "+fd);
